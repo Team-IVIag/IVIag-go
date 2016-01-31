cc9bbad45553e5650e4434d620bef3e13c9a90e9
cr0sh <ska827@naver.com>
2016-01-31 21:32:47 +0900

    Update

--- iviag.go
+++ iviag.go
@@ -3,12 +3,46 @@ package main
 import (
 	"flag"
 	"log"
+	"strconv"
 )
 
 func main() {
 	maxPics := flag.Int64("max", -1, "다운로드할 최대 이미지 수")
-	workers := flag.Uint64("worker", 5, "한 번에 동시에 다운로드할 이미지 수")
+	workers := flag.Uint64("worker", 5, "한 번에 동시에 다운로드할 만화 수")
+	fetchers := flag.Uint64("fetch", 1, "만화당 동시에 다운로드할 이미지 수")
 	flag.Parse()
 	mangas := flag.Args()
-	log.Println(mangas, *maxPics, *workers)
+
+	log.Println(mangas, *maxPics, *workers, fetchers)
+
+	targets := make([]uint64, len(mangas))
+	for i, manga := range mangas {
+		m, err := strconv.ParseUint(manga, 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		targets[i] = m
+	}
+
+	dl := new(Downloader)
+	dl.Init(*workers, *fetchers, *maxPics)
+	dl.Start(targets)
+
+	<-make(chan bool)
+}
+
+type Downloader struct {
+	Workers  []*Worker
+	workers  uint64
+	fetchers uint64
+	maxPics  int64
+}
+
+func (d *Downloader) Init(workers, fetchers uint64, maxPics int64) {
+	d.workers, d.fetchers, d.maxPics = workers, fetchers, maxPics
+	d.Workers = make([]*Worker, workers)
+}
+
+func (d *Downloader) Start(mangas []uint64) {
+
 }
--- worker.go
+++ worker.go
@@ -1,8 +1,60 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
+const (
+	MangaPrefix   = "http://marumaru.in/b/manga/"
+	ArchivePrefix = "http://www.mangaumaru.com/archives/"
+)
+
 type Worker struct {
+	ID       uint
+	Fetchers []*Fetcher
+	Targets  <-chan uint64
+	Report   chan<- Status
+	FRequest chan<- FetchRequest
+	FResult  <-chan FetchResult
+}
+
+func NewWorker(ID uint, fetchers uint64, tc <-chan uint64, rc chan<- Status) *Worker {
+	w := new(Worker)
+	w.ID = ID
+	w.Targets, w.Report = tc, rc
+	w.Fetchers = make([]*Fetcher, fetchers)
+	w.FRequest = make(chan FetchRequest, 30)
+	w.FResult = make(chan FetchResult, 30*fetchers)
+	return w
+}
+
+func (w *Worker) Download(manga uint64) {
+	url := fmt.Sprintf("%s%d", MangaPrefix, manga)
+	log.Print("Downloading:", url)
+}
+
+const (
+	FetchStart StatusID = iota
+	FetchComplete
+	FetchError
+	PicFetchStart
+	PicFetchComplete
+	PicFetchError
+	ListParseStart
+	ListParseError
+)
+
+type StatusID byte
+
+type Status struct {
+	ID   StatusID
+	Data string
+}
+
+type Fetcher struct {
 	ID      uint
-	Request <-chan [2]string //URL, filename
+	Request <-chan FetchRequest
 	Result  chan<- FetchResult
 }
 
