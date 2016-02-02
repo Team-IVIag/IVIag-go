4e5103b493b9164ef6b2d4d525058e8794e4537c
cr0sh <ska827@naver.com>
2016-02-02 22:18:52 +0900

    Dl/Worker/Fetcher 기반구조 완료

--- iviag.go
+++ iviag.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"strconv"
 )
 
@@ -23,6 +25,10 @@ func main() {
 		}
 		targets[i] = m
 	}
+	if *fetchers == 0 || *workers == 0 || *maxPics == 0 {
+		fmt.Println("잘못된 인자입니다.")
+		os.Exit(1)
+	}
 
 	dl := new(Downloader)
 	dl.Init(*workers, *fetchers, *maxPics)
@@ -36,13 +42,23 @@ type Downloader struct {
 	workers  uint64
 	fetchers uint64
 	maxPics  int64
+	target   chan<- uint64
+	report   <-chan Status
 }
 
 func (d *Downloader) Init(workers, fetchers uint64, maxPics int64) {
 	d.workers, d.fetchers, d.maxPics = workers, fetchers, maxPics
 	d.Workers = make([]*Worker, workers)
+	tc, rc := make(chan uint64, 30), make(chan Status, 30)
+	for i := range d.Workers {
+		d.Workers[i] = NewWorker(fetchers, tc, rc)
+	}
+	d.target = tc
+	d.report = rc
 }
 
 func (d *Downloader) Start(mangas []uint64) {
-
+	for _, manga := range mangas {
+		d.target <- manga
+	}
 }
--- worker.go
+++ worker.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"sync/atomic"
 )
 
 const (
@@ -10,28 +15,88 @@ const (
 	ArchivePrefix = "http://www.mangaumaru.com/archives/"
 )
 
+var workerID, fetchID uint32
+
 type Worker struct {
-	ID       uint
+	ID       uint32
 	Fetchers []*Fetcher
 	Targets  <-chan uint64
 	Report   chan<- Status
-	FRequest chan<- FetchRequest
-	FResult  <-chan FetchResult
+	FRequest chan FetchRequest
+	FResult  chan FetchResult
 }
 
-func NewWorker(ID uint, fetchers uint64, tc <-chan uint64, rc chan<- Status) *Worker {
+func NewWorker(fetchers uint64, tc <-chan uint64, rc chan<- Status) *Worker {
 	w := new(Worker)
-	w.ID = ID
+	w.ID = atomic.AddUint32(&workerID, 1)
 	w.Targets, w.Report = tc, rc
 	w.Fetchers = make([]*Fetcher, fetchers)
 	w.FRequest = make(chan FetchRequest, 30)
 	w.FResult = make(chan FetchResult, 30*fetchers)
+	for i := range w.Fetchers {
+		f := new(Fetcher)
+		f.ID = atomic.AddUint32(&fetchID, 1)
+		f.Request = w.FRequest
+		f.Result = w.FResult
+		w.Fetchers[i] = f
+		go w.Fetchers[i].fetch()
+	}
+	go w.Watch()
 	return w
 }
 
-func (w *Worker) Download(manga uint64) {
+func (w Worker) Watch() {
+	for target := range w.Targets {
+		w.Download(target)
+	}
+}
+
+func (w Worker) Download(manga uint64) {
 	url := fmt.Sprintf("%s%d", MangaPrefix, manga)
 	log.Print("Downloading:", url)
+}
+
+type Fetcher struct {
+	ID      uint32
+	Request <-chan FetchRequest
+	Result  chan<- FetchResult
+	Report  chan<- Status
+}
+
+func (f Fetcher) fetch() {
+	for req := range f.Request {
+		file, err := os.Create(req.filedir)
+		if err != nil {
+			f.Result <- FetchResult{
+				Ok:          false,
+				Description: "Could not create file: " + err.Error(),
+			}
+			continue
+		}
+		func() {
+			defer file.Close()
+			resp, err := http.Get(req.URL)
+			if err != nil {
+				f.Result <- FetchResult{
+					Ok:          false,
+					Description: "HTTP request error: " + err.Error(),
+				}
+				return
+			}
+			defer resp.Body.Close()
+			n, err := io.Copy(file, resp.Body)
+			if err != nil {
+				f.Result <- FetchResult{
+					Ok:          false,
+					Description: "File write error: " + err.Error(),
+				}
+			}
+			f.Result <- FetchResult{
+				Ok:          true,
+				Description: strconv.FormatInt(n, 10) + " bytes were written",
+			}
+		}()
+	}
 }
 
 const (
@@ -50,12 +115,6 @@ type StatusID byte
 type Status struct {
 	ID   StatusID
 	Data string
-}
-
-type Fetcher struct {
-	ID      uint
-	Request <-chan FetchRequest
-	Result  chan<- FetchResult
 }
 
 type FetchRequest struct {
