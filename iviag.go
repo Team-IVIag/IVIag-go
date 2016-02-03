d8545d6720319e665ba8719dc8a07ae87913c78e
cr0sh <ska827@naver.com>
2016-02-03 14:14:10 +0900

    만화별로 폴더 구분해서 저장

--- iviag.go
+++ iviag.go
@@ -95,11 +95,12 @@ func Get(url string) (string, error) { // From http://stackoverflow.com/question
 type Archive struct {
 	ID      uint64
 	Seq     uint64
+	Title   string
 	Subject string
 	Wait    *sync.WaitGroup
 }
 
-func GetArchives(manga uint64) (mangas []Archive, err error) {
+func GetArchives(manga uint64) (title string, mangas []Archive, err error) {
 	var doc *goquery.Document
 	doc, err = goquery.NewDocument(MangaPrefix + strconv.FormatUint(manga, 10))
 	if err != nil {
@@ -107,6 +108,10 @@ func GetArchives(manga uint64) (mangas []Archive, err error) {
 	}
 	links := make([]Archive, 0)
 	var seq uint64
+	title = Filter(doc.Find("div .subject").Find("h").First().Text(), "_")
+	if title == "" {
+		title = "Untitled"
+	}
 	doc.Find("div .content").Children().Find("a").Each(func(i int, s *goquery.Selection) {
 		if l, ok := s.Attr("href"); ok && strings.Index(l, ArchivePrefix) != -1 {
 			if link, err := strconv.ParseUint(l[strings.Index(l, ArchivePrefix)+len(ArchivePrefix):], 10, 64); err == nil {
@@ -122,13 +127,14 @@ func GetArchives(manga uint64) (mangas []Archive, err error) {
 				links = append(links, Archive{
 					ID:      link,
 					Seq:     seq,
+					Title:   title,
 					Subject: sub,
 				})
 				seq++
 			}
 		}
 	})
-	return links, nil
+	return title, links, nil
 }
 
 func GetPics(archive uint64) (rawhtml string, urls []string, err error) {
@@ -190,11 +196,12 @@ func (d *Downloader) Init(workers, fetchers uint64, maxPics int64) {
 func (d *Downloader) Start(mangas <-chan uint64, wg *sync.WaitGroup) {
 	for manga := range mangas {
 		log.Printf("Parsing archive list: %s%d", MangaPrefix, manga)
-		links, err := GetArchives(manga)
+		title, links, err := GetArchives(manga)
 		if err != nil {
 			log.Printf("Archive list parse error: %s", err.Error())
 			continue
 		}
+		os.MkdirAll(title, os.ModeDir)
 		d.Wait.Add(len(links))
 		for _, link := range links {
 			link.Wait = d.Wait
@@ -242,7 +249,7 @@ func NewWorker(fetchers uint64, tc <-chan Archive, rc chan<- Status) *Worker {
 
 func (w Worker) Watch() {
 	for target := range w.Targets {
-		dir := strconv.FormatUint(target.Seq, 10) + "_" + Filter(target.Subject, "_")
+		dir := target.Title + "/" + strconv.FormatUint(target.Seq, 10) + "_" + Filter(target.Subject, "_")
 		os.MkdirAll(dir, os.ModeDir)
 		log.Printf("Parsing archive: %s%d", ArchivePrefix, target.ID)
 		raw, imgs, err := GetPics(target.ID)
@@ -262,7 +269,7 @@ func (w Worker) Watch() {
 		for i, img := range imgs {
 			w.FRequest <- FetchRequest{
 				URL:     img,
-				FileDir: strconv.FormatUint(target.Seq, 10) + "_" + Filter(target.Subject+"/"+strconv.Itoa(i)+".jpg", "_"),
+				FileDir: target.Title + "/" + strconv.FormatUint(target.Seq, 10) + "_" + Filter(target.Subject+"/"+strconv.Itoa(i)+".jpg", "_"),
 				Wait:    wg,
 			}
 		}
